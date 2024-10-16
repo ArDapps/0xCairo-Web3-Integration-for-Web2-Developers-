099995c5cc2914df075af8d9304a2f233cd839e8
دابس بالعربي <arabicdapps@gmail.com>
2024-10-16 10:48:36 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ export default function Home() {
     <>
       <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black p-10 flex  flex-col justify-center items-center">
         <WalletConnectCard />
-        <BlockchainData />
       </div>
     </>
   );
