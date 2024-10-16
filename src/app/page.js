baea3c944fca7750beba913dbb3cce58fb3559bd
دابس بالعربي <arabicdapps@gmail.com>
2024-10-16 10:50:44 +0300

    remove testing

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,7 @@ import WalletConnectCard from "@/components/WallectConnectCard";
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black p-10 flex  flex-col justify-center items-center">
-        <WalletConnectCard />
-        <BlockchainData />
-      </div>
+      <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black p-10 flex  flex-col justify-center items-center"></div>
     </>
   );
 }
